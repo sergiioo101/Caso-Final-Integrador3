e3449f97951b27b46039cd7a7b317aa018b7075e
sergiioo101 <smartros@myuax.com>
2023-11-30 11:47:48 +0100

    estructura ColorConsole

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+struct ColorConsole
+{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+    static constexpr auto reset = "\033[0m";
+};
