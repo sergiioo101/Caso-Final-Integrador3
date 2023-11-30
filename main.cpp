87832ecd39167ee635d58a463ccb1854e1ce1e0d
sergiioo101 <smartros@myuax.com>
2023-11-30 11:52:07 +0100

    clase ConsoleBox

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
 struct ColorConsole
 {
     static constexpr auto fg_blue = "\033[34m";
     static constexpr auto bg_white = "\033[47m";
     static constexpr auto reset = "\033[0m";
 };
+
+class ConsoleBox
+{
+public:
+    void new_text() {/*...*/}
+    void set_text(const std::string &text)
+    {
+        std::cout << text << std::endl;
+    }
+};
+
+ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
