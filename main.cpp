716005165f7ba1dddce0a5ff0915dc95b91809b5
sergiioo101 <smartros@myuax.com>
2023-11-30 11:53:25 +0100

    funcion void load script

--- main.cpp
+++ main.cpp
@@ -21,3 +21,32 @@ public:
 };
 
 ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+
+void load_script(const std::string &filename, bool show_script = false)
+{
+    try
+    {
+        std::ifstream file(filename);
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error("Error al abrir el archivo: " + filename);
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string script = buffer.str();
+
+        if (show_script)
+        {
+            std::cout << ColorConsole::fg_blue << ColorConsole::bg_white << script << ColorConsole::reset << std::endl;
+        }
+
+        consoleBox->new_text();
+        consoleBox->set_text(script);
+    }
+    catch (const std::exception &e)
+    {
+        throw std::runtime_error("Error durante la lectura del archivo: " + std::string(e.what()));
+    }
+}
